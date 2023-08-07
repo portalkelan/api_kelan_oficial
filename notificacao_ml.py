7df60148b8b2d17a7d2d204c0ce4b0cff0035976
Thiago Leandro dos santos <info.sistemas.py@gmail.com>
2023-08-07 12:49:02 -0300

    Summary

--- notificacao_ml.py
+++ notificacao_ml.py
@@ -3,27 +3,19 @@ import time
 import json
 
 while True:
-    time.sleep(10)
-    def fazer_requisicao(url):
-        try:
-            resposta = requests.post(url)
-            resposta.raise_for_status()  # Verifica se houve algum erro na requisição
-            return resposta.text
-            
-        except requests.exceptions.RequestException as e:
-            print(f"Ocorreu um erro na requisição: {e}")
-            return None
-    
+    time.sleep(3)
+    try:
+        r = requests.post('https://kelanapi.azurewebsites.net/notification/teste')
+        print(r)
+        if r.status_code == 200:
+            p = r.json()
+            print(p)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Ocorreu um erro na requisição: {e}")
+
     # Exemplo de uso
-    url_servidor = "https://kelanapi.azurewebsites.net/notification/teste"
-    resposta_servidor = fazer_requisicao(url_servidor)
-    with open("json_perguntas/todas_notification.json", 'w') as file:
-        json.dump(resposta_servidor, file, indent=4)
-
-    if resposta_servidor:
-        print("Resposta do servidor:")
-        print(resposta_servidor)
-        print("########################################################")
-    else:
-        print("Não foi possível obter a resposta do servidor.")
-
+    #url_servidor = "https://kelanapi.azurewebsites.net/notification/teste"
+    #resposta_servidor = fazer_requisicao(url_servidor)
+    #with open("json_perguntas/todas_notification.json", 'w') as file:
+    #    json.dump(resposta_servidor, file, indent=4)
---
+++ teste.py
@@ -0,0 +1,28 @@
+import requests
+import time
+import json
+
+while True:
+    time.sleep(3)
+    def fazer_requisicao(url):
+        try:
+            resposta = requests.post(url)
+            resposta.raise_for_status()  # Verifica se houve algum erro na requisição
+            return resposta.text
+            
+        except requests.exceptions.RequestException as e:
+            print(f"Ocorreu um erro na requisição: {e}")
+            return None
+    
+    # Exemplo de uso
+    url_servidor = "https://kelanapi.azurewebsites.net/notification/teste"
+    resposta_servidor = fazer_requisicao(url_servidor)
+    with open("json_perguntas/todas_notification.json", 'w') as file:
+        json.dump(resposta_servidor, file, indent=4)
+
+    if resposta_servidor:
+        print("Resposta do servidor:")
+        print("########################################################")
+    else:
+        print("Não foi possível obter a resposta do servidor.")
+

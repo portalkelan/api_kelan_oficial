7c90418da09f873de7dcdadeddbc7f7df3033d2c
Thiago Leandro dos santos <info.sistemas.py@gmail.com>
2023-09-06 13:13:04 -0300

    summary

--- teste_lanna.py
+++ teste_lanna.py
@@ -13,7 +13,7 @@ def converter_formato_com_hora(data_iso):
     data_br = data_objeto.strftime('%d/%m/%y %H:%M:%S')
     return data_br
 
-openai.api_key = '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'  # Sua chave da API OpenAI
+openai.api_key = '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'  # Sua chave da API OpenAI
 
 ## Cria o sistema de fila a partir do id da pergunta
 previous_question_id = ""

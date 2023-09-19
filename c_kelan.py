4ec922cb31c3c339a281756e3190c7d8c36166ec
Thiago Leandro dos santos <133038648+portalkelan@users.noreply.github.com>
2023-09-19 14:13:08 -0300

    summary

--- c_kelan.py
+++ c_kelan.py
@@ -13,7 +13,7 @@ def converter_formato_com_hora(data_iso):
     data_br = data_objeto.strftime('%d/%m/%y %H:%M:%S')
     return data_br
 
-openai.api_key = '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'  # Sua chave da API OpenAI
+openai.api_key = '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'  # Sua chave da API OpenAI
 
 ## Cria o sistema de fila a partir do id da pergunta
 previous_question_id = ""

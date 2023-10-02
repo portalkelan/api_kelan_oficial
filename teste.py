6f8c46f5cb1f3057c4675bd2a72ccab09a9f27d1
Thiago Leandro dos santos <133038648+portalkelan@users.noreply.github.com>
2023-10-02 09:53:58 -0300

    summary

--- teste.py
+++ teste.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 ### Chave da API Open_AI
-openai.api_key = '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'
+openai.api_key = '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'
 
 # Link de Reclamação
 link_reclamaçao = 'myaccount.mercadolivre.com.br/my_purchases/list'
@@ -58,7 +58,7 @@ def fetch_and_process_data():
             temperature = 0
             max_tokens = 256
             messages = [
-                {"role": "system", "content": f"Atue como um profissional de atendimento ao cliente... [restante da mensagem]"}
+              {"role": "system", "content":f"Atue como um profissional de atendimento ao cliente e responda as perguntas sobre os produtos da loja KELAN MOVEIS na plataforma Mercado Livre.Responda as perguntas dos clientes utilizando o catalogo como parametro de resposta. Caso a resposta para a pergunta não esteja no prompt, cole a seguinte mensagem 'Olá, infelizmente não encontrei uma resposta para a sua pergunta nos meus dados de treinamento, por favor, entre em contato conosco através das mensagens do Mercado Livre neste link: {link_reclamaçao}, será um prazer ajudá-lo!'.RECLAMAÇÕES:'Olá, infelizmente não encontrei uma resposta para a sua pergunta nos meus dados de treinamento, por favor, entre em contato conosco através das mensagens do Mercado Livre neste link: {link_reclamaçao}, será um prazer ajudá-lo!' Horário de atendimento: Seg à Sex das 8h às 18h. Em caso de problemas com o produto, peça que o cliente não abra uma  reclamação e tente entrar em contato conosoco. Não responda perguntas sobre preços, não responda perguntas sobre as notas fiscais, não invente respostas, siga as informações da descrição à risca! NOME DO PRODUTO: {itemName}, DESCRIÇAO: {itemDescription}"}
             ]
 
             if seller_id == kelan_id: 
@@ -136,7 +136,7 @@ def insert_into_database(question_id, seller_id, date_created, item_id, question
         con = mysql.connector.connect(host='localhost', database='kelan', user='root', password='')
         if con.is_connected():
             cursor = con.cursor()
-            query = "INSERT IGNORE INTO api_kelan_mlb (question_id, seller_id, date_created, item_id, question_text, itemName, item_Description, response_json) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+            query = "INSERT IGNORE INTO api_kelan_mlb (question_id, seller_id, date_created, item_id, question_text, itemName, item_Description, response_json, foi_respondida) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
             values = (question_id, seller_id, date_created, item_id, question_text, itemName, itemDescription, reply, foi_respondida)
             cursor.execute(query, values)
             con.commit()
@@ -154,4 +154,4 @@ while True:
     if request_queue:
         fetch_and_process_data()
         request_queue.popleft()
-    time.sleep(60)
+    time.sleep(180)

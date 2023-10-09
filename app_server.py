531e1979cac79a8d1ea54052354bc3be6d900621
Thiago Leandro dos santos <133038648+portalkelan@users.noreply.github.com>
2023-10-09 13:54:15 -0300

    summary

--- app_server.py
+++ app_server.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # Definindo a rota '/data' que aceita requisições GET
-@app.route('/data', methods=['POST'])
+@app.route('/data', methods=['GET'])
 def get_data():
     # Aqui você pode processar ou buscar os dados que deseja retornar
     data = {
@@ -13,4 +13,5 @@ def get_data():
     return jsonify(data), 200
 
 if __name__ == '__main__':
-    app.run(debug=False, port=8050, host='172.20.20.33')
+    app.run(debug=False, host='172.20.20.37')
+    
